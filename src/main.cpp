a19cb60f36f3eb4f7e29377c86d1758c3cd5c17f
Balaji Saravana Gupta <balaji.saravana.revathy@gmail.com>
2025-07-23 09:45:14 -0700

    fix: Added main initialization code

---
+++ src/main.cpp
@@ -0,0 +1,161 @@
+#include "main.h"
+#include "pros/misc.h"
+#include "api.h"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "liblvgl/llemu.hpp"
+#include "lemlib/chassis/chassis.hpp"
+#include "bezier.hpp"
+#include "file-parser.hpp"
+#include "motion-profiler.hpp"
+#include "motion-profiling.hpp"
+#include "motion-utils.hpp"
+#include "ramsete.hpp"
+#include "printer.hpp"
+
+pros::Controller controller(CONTROLLER_MASTER);
+
+pros::MotorGroup rightMotors({-11,2,1});
+pros::MotorGroup leftMotors({-16,14,-13});
+
+lemlib::Drivetrain drivetrain(&leftMotors, &rightMotors, 10, 3.25, 450, 2);
+
+pros::IMU imu(3);
+
+pros::Rotation horizontal_tracking_sensor(12);
+pros::Rotation vertical_tracking_sensor(15);
+
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_tracking_sensor, 3.75, 9/2.54, 1);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_tracking_sensor, 3.75, 7/100/2.54,1);
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, nullptr, &horizontal_tracking_wheel, nullptr, &imu);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sensors);
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous(int argc, char* argv[]) {
+    std::vector<std::vector<Point>> controlPoints;
+	std::vector<std::vector<KeyframeVelocitiesXandY>> keyFrameVelocityList;
+	std::string filename = "path-points_txt";
+
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+
+            if (arg == "--file" && i + 1 < argc) {
+                filename = argv[i + 1];
+                ++i; // Skip next because it's the filename
+            }
+        }
+
+        if (!filename.empty()) {
+            std::cout << "Using file: " << filename << std::endl;
+        } else {
+            std::cerr << "File empty!\n";
+            exit;
+        }
+        loadPaths(filename, controlPoints, keyFrameVelocityList);
+	printVels(controlPoints, keyFrameVelocityList, true);
+        
+
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+
+
+void opcontrol() {
+    while (true)
+    {
+        int rightControl = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+        int leftControl = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+
+        chassis.tank(leftControl, rightControl, false);
+
+        pros::delay(20);
+    }
+}
