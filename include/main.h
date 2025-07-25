0a03406a0fdd27d48232ce4da9651d75e5a12e5d
Bal207 <80231343+Bal207@users.noreply.github.com>
2025-07-25 09:33:03 -0700

    feat: add intake/agitator subystem (#8)
    * Add drivetrain code and setup ports
    
    * feat: Tune PID for programming drivetrain
    
    * Update ports and add curves to drivetrain
    
    * Add drivetrain code for competition drivetrain
    
    * fix: Add Trigger functionality and fix header file mismatches
    
    * Fix: Fix header file mismatch
    
    * Fix: libunit headers and integration
    
    * Fix: fix ports mismatch and toggle commands on intake
    
    * fix: remove unnecessary files
    
    * feat: add intake/agitator subystem #7
    
    * Adding units.hpp file
    
    * chore: update libraries and clean up headers
    
    ---------
    
    Co-authored-by: Centei <115051114+Centei@users.noreply.github.com>

--- include/main.h
+++ include/main.h
@@ -40,6 +40,8 @@
  * You should add more #includes here
  */
 //#include "okapi/api.hpp"
+#include "command/includes.h"
+#include "units/units.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
--- src/main.cpp
+++ src/main.cpp
@@ -1,132 +1,44 @@
 #include "main.h"
-#include "lemlib/api.hpp" // IWYU pragma: keep
-#include "lemlib/chassis/chassis.hpp"
-#include "lemlib/chassis/trackingWheel.hpp"
-#include "liblvgl/llemu.hpp"
-#include "pros/misc.h"
+#include "command/commandController.h"
+#include "command/commandScheduler.h"
+#include "robot/globals.h" // Assuming 'chassis' is declared in here
+#include "robot/intake.h"  // Your intake subsystem
+#include "units/units.hpp"
 
+using namespace units;
 
-pros::Controller controller(CONTROLLER_MASTER);
+// Global Objects
+CommandController primary(pros::E_CONTROLLER_MASTER);
+Intake *intake;
 
-pros::MotorGroup rightMotors({-11,2,1});
-pros::MotorGroup leftMotors({-16,14,-13});
-
-lemlib::Drivetrain drivetrain(&leftMotors, &rightMotors, 10, 3.25, 450, 2);
-
-pros::IMU imu(3);
-
-pros::Rotation horizontal_tracking_sensor(12);
-pros::Rotation vertical_tracking_sensor(15);
-
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_tracking_sensor, 3.75, 9/2.54, 1);
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_tracking_sensor, 3.75, 7/100/2.54,1);
-lemlib::OdomSensors sensors(&vertical_tracking_wheel, nullptr, &horizontal_tracking_wheel, nullptr, &imu);
-
-// lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              3, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
-);
-
-// angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
-);
-
-lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sensors);
-
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-    pros::lcd::initialize(); // initialize brain screen
-    chassis.calibrate(); // calibrate sensors
-    // print position to brain screen
-    pros::Task screen_task([&]() {
-        while (true) {
-            // print robot location to the brain screen
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            // delay to save resources
-            pros::delay(20);
-        }
-    });
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
-
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-
-
-
-void opcontrol() {
-    while (true)
-    {
-        int rightControl = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-        int leftControl = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-
-        chassis.tank(leftControl, rightControl, false);
-
-        pros::delay(20);
+// Command scheduler loop
+[[noreturn]] void update_loop() {
+    while (true) {
+        auto start_time = pros::millis();
+        CommandScheduler::run();
+        pros::c::task_delay_until(&start_time, 10);
     }
 }
+
+void initialize() {
+    chassis.calibrate(); // calibrate sensors
+    pros::Task commandSchedulerTask(update_loop);
+    intake = new Intake(pros::Motor(20));
+    CommandScheduler::registerSubsystem(intake, intake->pctCommand(0.0));
+    primary.getTrigger(DIGITAL_L1)->whileTrue(intake->pctCommand(-1.0));
+    primary.getTrigger(DIGITAL_L2)->whileTrue(intake->pctCommand(1.0));
+    primary.getTrigger(DIGITAL_A)->whileTrue(intake->pctCommand(-1.0)
+                                                 ->withTimeout(300_ms)
+                                                 ->andThen(intake->pctCommand(1.0)->withTimeout(300_ms))
+                                                 ->repeatedly());
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {
+    // Your autonomous code will go here
+}
+
+void opcontrol() {}
